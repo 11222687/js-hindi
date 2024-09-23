0fa113d3e1186d5a0d1e0b5d2560196ad2a8628e
Rahul Kumar <160120718+11222687@users.noreply.github.com>
2024-09-23 05:05:44 +0000

    conatiner config commit

---
+++ String/String_Concatenation.js
@@ -0,0 +1,5 @@
+let str1='Java';
+let str2='Script';
+//Concatenation 
+let result=str1+str2;
+console.log("hello " +result);
---
+++ String/finding_length.js
@@ -0,0 +1,4 @@
+let str='javaScript'
+//finding the length of the String
+let len=str.length
+console.log("the lrngth of the String is: " +len);
