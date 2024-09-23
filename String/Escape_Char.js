4c54bbee9be9fd9814afa2c6a7f9df9a14f6958d
Rahul Kumar <160120718+11222687@users.noreply.github.com>
2024-09-23 20:09:56 +0000

    container config done

---
+++ String/Escape_Char.js
@@ -0,0 +1,8 @@
+const str1="\pw\' is a learning portal";
+const str2="\"pwskills\" is learning portal";
+const st3="\\physicswallah\\ is a learning portal";
+
+console.log(str1);
+console.log(str2);
+console.log(st3);
+
