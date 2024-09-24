f5322ef946ac6862f9a4dfd55db3a0295458de75
Rahul Kumar <160120718+11222687@users.noreply.github.com>
2024-09-24 04:46:49 +0000

    container config done

---
+++ 01_Basic/04_datatype-summary.js
@@ -0,0 +1,39 @@
+//primitive
+//7 type:- String, Boolean, Number, null,undefine,Symbol, BigInt
+
+const score=100;
+const scoreValue=100.89;
+
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id===anotherId);
+
+const bigNumber=2345667224354876677n;
+console.log(bigNumber);
+
+//Reference and non-primitive
+
+//Array, Object, Functions
+
+const heros=["saktiman" ,"naagraj" ,"doga"];
+
+{
+    name: "Rahul"
+    age: 23
+    rollno: 11222687
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+
+}
+//console.log(myFunction);
+console.log(typeof heros);
+console.log(typeof bigNumber);
+console.log(typeof id);
+
